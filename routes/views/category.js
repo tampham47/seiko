0ca1e44f849db8d468eb8bca39cdab136bb6f36c
Tam Pham <tampham47@live.com>
2017-06-23 21:00:32 +0700

    get data for category

--- routes/views/category.js
+++ routes/views/category.js
@@ -9,6 +9,15 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	// locals.section = 'home';
 
+  view.on('init', function(next) {
+    var q = keystone.list('Product').model.find();
+    q.exec(function(err, respone) {
+      console.log('data', respone.length);
+      locals.data = respone;
+      next();
+    });
+  });
+
 	// Render the view
 	view.render('category');
 };
