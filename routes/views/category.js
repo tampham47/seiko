7c2fd6536c7e4ffc4aedd4a1b8ef2e3f7fe8813a
Tam Pham <tampham47@live.com>
2017-06-26 07:18:56 +0700

    add id to category page

--- routes/views/category.js
+++ routes/views/category.js
@@ -2,12 +2,12 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	// locals.section = 'home';
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  // locals.section = 'home';
 
   view.on('init', function(next) {
     var q = keystone.list('Product').model.find();
